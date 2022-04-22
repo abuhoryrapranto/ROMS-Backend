85ed5b360c66baed43ed403a064e8329e7546a84
mahpranto <pranto@kludioasia.com>
2022-04-22 18:04:12 +0600

    Update Index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 require('dotenv').config()
 const port = process.env.PORT || 5000
-const { auth } = require('./middlewares/auth')
-const authRoutes = require('./routes/auth')
-const menuRoutes = require('./routes/menu')
-const categoryRoutes = require('./routes/category')
-const userRoutes = require('./routes/user')
-const orderRoutes = require('./routes/order')
-const cashBoxRoutes = require('./routes/cashbox')
+const { auth } = require('./app/middlewares/auth')
+const authRoutes = require('./app/routes/auth')
+const menuRoutes = require('./app/routes/menu')
+const categoryRoutes = require('./app/routes/category')
+const userRoutes = require('./app/routes/user')
+const orderRoutes = require('./app/routes/order')
+const cashBoxRoutes = require('./app/routes/cashbox')
 
 app.use(cors());
 app.use(cookieParser());
